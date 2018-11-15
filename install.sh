85c7e45ff02254b7bbe57d32916be6e624e398b4
Brice Gagnage <brice.gagnage@renault.com>
2018-11-15 11:06:07 +0100

    fix

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -eu
 
 cd $HOME
 LOCAL_DIR=$HOME/local
-mkdir $LOCAL_DIR
+mkdir -p $LOCAL_DIR
 echo "installing prerequisites..."
-sudo apt update && sudo apt install -y zsh ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip jq curl
+sudo apt update && sudo apt install -y zsh ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip jq curl tmux
 
 echo "installing oh-my-zsh..."
-$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 cat > $HOME/.zshrc <<- EOM
 # zshrc alienware
 . $HOME/Miaoou/conf/.zshrc.common
@@ -15,7 +16,7 @@ EOM
 
 echo "compiling and installing neovim..."
 git clone https://github.com/neovim/neovim.git
-NB_CPU=lscpu -e=cpu -J|jq -r '.["cpus"] | length'
+NB_CPU=$(($(lscpu -e=cpu|wc -l) - 1))
 $(cd neovim && make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$LOCAL_DIR -DCMAKE_BUILD_TYPE=Release" -j$NB_CPU install)
 NVIM_CONFIG_DIR=$HOME/.config/nvim
 mkdir -p $NVIM_CONFIG_DIR 
